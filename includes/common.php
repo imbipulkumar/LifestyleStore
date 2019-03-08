0c727883cf89bb25895be80a6c23e5d27a3de58b
Bipul Kumar <imbipulkumar@users.noreply.github.com>
2019-03-09 01:21:08 +0530

    added DB connection and made necessary composer changes

--- includes/common.php
+++ includes/common.php
@@ -1,4 +1,4 @@
 <?php
-$con= mysqli_connect("localhost","root","","lifestylestore") or die(mysqli_error($con));
+$con= mysqli_connect("us-cdbr-gcp-east-01.cleardb.net","b0817afb9c1cc2","4376fd75","gcp_0bee089c14d4afa395d0") or die(mysqli_error($con));
 session_start();
 ?>
